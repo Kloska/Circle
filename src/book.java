bee6e02f6380df6525c6a2390f52258098406b2a
Kloska <56342396+Kloska@users.noreply.github.com>
2021-09-16 13:41:22 +0200

    book

---
+++ src/book.java
@@ -0,0 +1,13 @@
+public class book {
+    private String title;
+    private Author author;
+    private double price;
+    private int pages;
+
+    public book(String title, Author author, double price, int pages) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.pages = pages;
+    }
+}
